7710458084ea5c40ebc7d0bd5f81f64babeffb17
1MrLi <112091702+1MrLi@users.noreply.github.com>
2022-09-17 20:36:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ export const config = {
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: false,
+  IS_SHOW_COLOR: true,
 
   // 功能开关,打开：true，关闭：false
   SWITCH: {
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ob0Yj6cwjhVRAlXT411aPzNqGfhA",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "lv3vdb4vo5zMDE0I1gUvAoBss0lY3QJ9SYFMyoBsDV0",
+      useTemplateId: "mqSDlBpaxSr8HWZdk9XWCaw4vl2pqdU6Y6uNPbVJY6Y",
       // 所在省份
       province: "陕西",
       // 所在城市
